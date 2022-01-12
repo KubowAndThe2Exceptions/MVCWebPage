262d06289253c862d3950b3439febd73196c8cc6
Ryan Kubow <ryankubow@gmail.com>
2022-01-11 20:59:40 -0800

    Constructor to set ApplicationDbContext.  Set up Index action

--- MVCDemo/Controllers/SearchUsersController.cs
+++ MVCDemo/Controllers/SearchUsersController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVCDemo.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,16 @@ namespace MVCDemo.Controllers
 {
     public class SearchUsersController : Controller
     {
+        private ApplicationDbContext _appDb;
+        public SearchUsersController(ApplicationDbContext appDb)
+        {
+            _appDb = appDb;
+        }
         // GET: SearchUsers
         public ActionResult Index()
         {
-            return View();
+
+            return View(_appDb.Users.ToList());
         }
     }
 }
