386ef069bfc6a130608013a129a36787d47cd9b5
Ryan Kubow <ryankubow@gmail.com>
2022-01-11 20:40:50 -0800

    ApplicationUser Model/Db Model changes, also created extension for IPrincipal to access some custom claims in views

---
+++ MVCDemo/Extensions/IPrincipalExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web;
+
+namespace MVCDemo.Extensions
+{
+    public static class IPrincipalExtension
+    {
+        public static String GetFirstName(this IPrincipal principal)
+        {
+            var claimsPrincipal = principal as ClaimsPrincipal;
+
+            var personFirstNameClaim = claimsPrincipal.Claims.FirstOrDefault(c => c.Type == "FirstName");
+            if (personFirstNameClaim != null)
+            {
+                return personFirstNameClaim.Value;
+            }
+
+            return String.Empty;
+        }
+    }
+}
--- MVCDemo/Models/IdentityModels.cs
+++ MVCDemo/Models/IdentityModels.cs
@@ -9,11 +9,17 @@ namespace MVCDemo.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
+            userIdentity.AddClaim(new Claim("FirstName", this.FirstName));
+            userIdentity.AddClaim(new Claim("LastName", this.LastName));
             return userIdentity;
         }
     }
